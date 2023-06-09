a0265eac3cdb065e84fe0fe0a59ac7a12b15ddaa
Gary Leung <pleung0827@gmail.com>
2023-06-08 20:55:33 -0400

    Finished test cases and Javadocs

--- app/src/main/java/com/example/garysandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/garysandroidlabs/MainActivity.java
@@ -1,27 +1,52 @@
 package com.example.garysandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.content.Context;
-//import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageButton;
-//import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.garysandroidlabs.databinding.ActivityMainBinding;
 
+/**
+ * The main activity java class of the activity_main.xml
+ * @author Kin Man Leung
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
 
+
     private ActivityMainBinding variableBinding;
-    private MainViewModel model;
+//    private MainViewModel model;
+
+    /**
+     * A function to check the password contains at least an uppercase, a lowercase,
+     * a number and a special character.
+     * @param pw The String object that we are checking
+     * @return  Return true if the password is complex enough
+     */
+    private boolean checkPasswordComplexity(String pw){
+        if(!pw.matches(".*[A-Z].*")){
+            Toast.makeText(this, "missing an upper case letter", Toast.LENGTH_SHORT);
+            return false;
+        }
+        else if(!pw.matches(".*[a-z].*")){
+            Toast.makeText(this, "missing a lower case letter", Toast.LENGTH_SHORT);
+            return false;
+        }
+        else if(!pw.matches(".*\\d.*")){
+            Toast.makeText(this, "missing a number", Toast.LENGTH_SHORT);
+            return false;
+        }
+        else if(!pw.matches(".*[#$%^&*!@?].*")){
+            Toast.makeText(this, "missing a Special Character (#$%^&*!@?)", Toast.LENGTH_SHORT);
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,66 +57,24 @@ public class MainActivity extends AppCompatActivity {
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        //instaniate a view model
-        model = new ViewModelProvider(this).get(MainViewModel.class);
-
-        //name all elements
-        TextView mytext = variableBinding.textView;
-        Button mybutton = variableBinding.mybutton;
-        EditText myedit = variableBinding.editText;
-        Switch myswitch = variableBinding.mySwitch;
-        CheckBox mycheckbox = variableBinding.checkBox;
-        RadioButton myradiobutton = variableBinding.radioButton;
-        ImageButton myimagebutton = variableBinding.imageButton;
-        //ImageView myimageview = variableBinding.imageButton;
+        /** This holds the text at the centre of the screen*/
+        TextView tV = variableBinding.textView;
+        /** This holds the login in button*/
+        Button loginBtn = variableBinding.loginBtn;
+        /** This hold the input of the password from the user */
+        EditText eT = variableBinding.editText;
 
         //click button to show text from edittext to text view
-        mybutton.setOnClickListener(click -> {
-            String word = myedit.getText().toString();
-            model.editString.postValue(word);
-            model.editString.observe(this, s ->
-                mytext.setText("Your edit text has " + s)
-            );
+        loginBtn.setOnClickListener(click -> {
+            String passwd = eT.getText().toString();
+            if(checkPasswordComplexity(passwd)){
+                tV.setText("Your password is complex enough");
+            }
+            else{
+                tV.setText("You shall not pass!");
+            }
         });
-
-        //compound button listener
-        mycheckbox.setOnCheckedChangeListener((btn, isChecked) ->
-                model.isSelected.postValue(isChecked)
-        );
-        myradiobutton.setOnCheckedChangeListener((btn, isChecked) ->
-                model.isSelected.postValue(isChecked)
-        );
-        myswitch.setOnCheckedChangeListener((btn, isChecked) ->
-                model.isSelected.postValue(isChecked)
-        );
-
-        //button view model
-        model.isSelected.observe(this, selected -> {
-            mycheckbox.setChecked(selected);
-            myradiobutton.setChecked(selected);
-            myswitch.setChecked(selected);
-
-            Context context = getApplicationContext();
-            CharSequence text = "3 buttons are Selected!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        });
-
-
-        //click imagebutton to show the toast image
-        myimagebutton.setOnClickListener(click -> {
-            int width = myimagebutton.getWidth();
-            int height = myimagebutton.getHeight();
-
-            Context context = getApplicationContext();
-            CharSequence text = "The width = " + width + " and height = " + height;
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        });
-        //
     }
 }
+
+
